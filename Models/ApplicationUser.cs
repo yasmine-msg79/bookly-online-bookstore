585e8d26ba3d1a9ea6f6708d6873b6852136ed81
hanasoliman72 <hanasmsalah105@gmail.com>
2025-08-01 12:38:21 +0300

    Add super admin

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
-    public enum Role { Admin, Guest}
+    public enum Role { SuperAdmin, Admin, Guest}
     public enum Gender { Male, Female}
     public class ApplicationUser : IdentityUser
     {
--- obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e73a777eec71e40d5e1b828525faf04276c2f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
