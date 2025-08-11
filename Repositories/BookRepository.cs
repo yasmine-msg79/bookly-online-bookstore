61c834405a257939d65a0e25819a0d17adc34e8e
yasmine-msg79 <111207118+yasmine-msg79@users.noreply.github.com>
2025-08-11 14:54:30 +0300

    initial book repositories

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+using BookStore.Data;
+using BookStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Book GetBookById(int id)
+        {
+            return _context.Books.FirstOrDefault(b => b.Id == id) ?? throw new KeyNotFoundException($"Book with ID {id} not found.");
+        }
+
+        public void AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+        }
+
+        public void DeleteBook(int id)
+        {
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
